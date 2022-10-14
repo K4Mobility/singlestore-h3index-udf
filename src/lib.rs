dcf19d9d491952819264b000f893468f109e6f04
Ravi Kiran Eticala <ravi.eticala@k4mobility.com>
2022-10-13 22:03:58 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+wit_bindgen_rust::export!("h3index.wit");
+struct H3index;
+use geo_types::Coordinate;
+use h3ron::{H3Cell, Index};
+
+
+impl h3index::H3index for H3index {
+    fn h3index(lat: f64, lon: f64, res: u8) -> u64 {
+		return H3Cell::from_coordinate(Coordinate::from((lat, lon)), res).unwrap().h3index();
+    }
+}
